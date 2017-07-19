8c46a3e5cfa1b58bef1cdf0ee5410071c6962d66
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-07-19 16:54:36 +0200

    Selecting a unit

--- main.cpp
+++ main.cpp
@@ -11,7 +11,13 @@ int main() {
 	// on crée la fenêtre
 	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Tilemap");
 	window.setFramerateLimit(24);
+
+	sf::Text selectedText;
+	bool hasSelection = false;
+	std::vector<Unit *> units;
 	SwordsmanUnit swordsman;
+	units.push_back(&swordsman);
+
 	swordsman.load();
 
 
@@ -37,26 +43,51 @@ int main() {
 	sf::Text text = TextBuilder::Instance().build("Hit the space bar");
 	text.setCharacterSize(32);
 
+	sf::Vector2i clickPos;
+	sf::Vector2f tmpPos;
+	sf::Drawable *selected;
+
 	while (window.isOpen()) {
 
 		sf::Event event;
 		while (window.pollEvent(event)) {
 
-			if (event.type == sf::Event::Closed)
-				window.close();
-
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::MouseButtonPressed:
+					clickPos = sf::Mouse::getPosition(window);
+					for (int i = 0; i < units.size(); i++) {
+						tmpPos = units[i]->getSprite().getPosition();
+						if (tmpPos.x - clickPos.x < tileWidth
+						    && tmpPos.y - clickPos.y < tileHeight) {
+							selectedText = TextBuilder::Instance().build("SELECTED");
+							selectedText.setPosition(tmpPos.x,
+							                         tmpPos.y);
+							hasSelection = true;
+							break;
+						}
+					}
+					break;
+				default:
+					break;
+			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
 				int randX = rand() % 3 + (-1);
 				int randY = rand() % 3 + (-1);
 
 				swordsman.move(randX, randY);
 				sf::sleep(sf::milliseconds(100));
+				hasSelection = false;
 			}
 
 			window.clear(sf::Color::Black);
 			//window.draw(text);
 			window.draw(map);
 			window.draw(swordsman);
+			if (hasSelection)
+				window.draw(selectedText);
 			window.display();
 		}
 	}
